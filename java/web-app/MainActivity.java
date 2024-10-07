59fade3f25a08848a6bbe2f1f7ab6a59ef6a524d
v02qabs <takesue090@gmail.com>
2024-10-07 17:55:06 +0900

    index.html add android dev a link

---
+++ java/web-app/MainActivity.java
@@ -0,0 +1,12 @@
+import java.io.*;
+ 
+public class MainActivity{
+	public MainActivity(){
+		}
+	public static void main(String[] args)
+	{
+		System.out.println("Hello web-app.");		
+		new initClass();
+	}
+}
+
---
+++ java/web-app/initClass.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.lang.*;
+import hiro.*;
+public class initClass{
+	public String login_user;
+	public String login_pass;
+
+	public initClass(){
+		init();
+	}
+	private void init(){
+		hu7System.out.println("初期化します。");
+
+		
+		System.out.println("user name please , input");
+		System.out.println("user pass words.");
+		
+ 	}
+}
+
