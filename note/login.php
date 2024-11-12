f66162b2804cdc6cfb28a143108dd7d5951d0bcd
v02qabs <takesue090@gmail.com>
2024-11-12 22:33:20 +0900

    2024-11-12-22:33

--- note/login.php
+++ note/login.php
@@ -1,21 +1,14 @@
 <?php
-class login{
-	public function(){
-
 	echo("ようこそ、hironoteへ。\n");
 	echo("ok");
 	echo("<br/>");
 	echo($_GET['pass']);
 	$db = new PDO('mysql:host=localhost;dbname=hirofumi090', 'hirofumi090', 'hirofumi090Abc');
 	$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-	$stmt = $db -> query("SELECT * FROM login");
+	$stmt = $db -> query("SELECT * FROM login where pass=pass ");
 	foreach($stmt as $row){
-		echo $row['pass'];
-	
-	}
+		echo("$stmtお疲れ様でした。");
 
 	}
-}
-
 	echo("login.php");
 ?>
