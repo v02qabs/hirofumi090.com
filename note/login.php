1ec8f93a50898f1a910555abe031f38de75d7501
v02qabs <takesue090@gmail.com>
2024-11-25 09:34:49 +0900

    add the app ppm

--- note/login.php
+++ note/login.php
@@ -5,9 +5,11 @@
 	echo($_GET['pass']);
 	$db = new PDO('mysql:host=localhost;dbname=hirofumi090', 'hirofumi090', 'hirofumi090Abc');
 	$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-	$stmt = $db -> query("SELECT * FROM login where pass=pass ");
+	$stmt = $db -> query("SELECT pass FROM login where pass='1210'");
 	foreach($stmt as $row){
-		echo("$stmtお疲れ様でした。");
+		echo("Hello.$stmt様");
+		echo("
+
 
 	}
 	echo("login.php");
