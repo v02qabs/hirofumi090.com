a03ee771144731f2a825c15147190d12d9bae309
v02qabs <takesue090@gmail.com>
2025-01-08 23:57:53 +0900

    2025-01-08

---
+++ myapp/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE HTML>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>PC技術・ドキュメント</title>
+	<link rel="stylesheet" href="layout.css" type="text/css">
+</head>
+
+<body>
+
+<div class="wrapper">
+<header class="header">
+	<nav class="g-nav-03">
+		<ul class="clearfix">
+		<li class="home"><a href="https://hirofumi090.chobi.net"><span></span>HOME</a></li>
+			<li class="flower"><a href=""><span></span>言語</a>
+				<ul class="second-level">
+					<li><a href="https://hirofumi090.chobi.net/java/index.html">java</a></li>
+					<li><a href="https://hirofumi090.chobi.net/myapp/index.html">私の作ったアプリ</a></li>
+					</ul>
+			</li>
+			
+			<li class="gift"><a href="info.html"><span></span>情報</a>
+			</li>
+			<li class="shop"><a href="#"><span></span>お店情報</a></li>
+			<li class="contact"><a href="#"><span></span>お問い合せ</a></li>
+			<li class="shop"><a href="./blog/index.html"><span></span>blog</a></li>
+	</nav>
+</header>
+
+
+<h1>自分が作ったアプリたち</h1>
+・ここでは、作成したアプリについて、解説していきます。
+<br/>
+<div id="menu1">
+<a href="./login.html">memo note</a><br/>
+
+</div>
+
+<style>
+#menu1
+{
+	padding: 10px;
+	width: 200px;
+	border: ridge 3px red;
+}
+
+</style>
+
+
+
+</body>
+</html>
